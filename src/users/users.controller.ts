d1cf2e731c0186b51b6baedab9d0bf07911fc6cd
Vlad <vladyslav.braslavskyi@flip.shop>
2024-04-01 14:38:41 +0200

    chore: format users resources code styles

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,35 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  public constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  public create(@Body() createUserDto: CreateUserDto): string {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  public findAll(): string {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  public findOne(@Param('id') id: string): string {
     return this.usersService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  public update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto): string {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  public remove(@Param('id') id: string): string {
     return this.usersService.remove(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
+  public create(createUserDto: CreateUserDto): string {
     return 'This action adds a new user';
   }
 
-  findAll() {
+  public findAll(): string {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  public findOne(id: number): string {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  public update(id: number, updateUserDto: UpdateUserDto): string {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  public remove(id: number): string {
     return `This action removes a #${id} user`;
   }
 }
