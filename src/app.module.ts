d2ba458aa78c5a65e92e17612b921d60b164be83
Vlad <vladyslav.braslavskyi@flip.shop>
2024-04-01 12:49:55 +0200

    feat: generate users resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
+  imports: [UsersModule],
 })
 export class AppModule {}
